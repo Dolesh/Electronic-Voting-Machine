72483f85a09fd999850cabf57b27479256bb59d0
Dolesh <54908648+Dolesh@users.noreply.github.com>
2021-04-11 16:30:05 +0530

    Add files via upload

---
+++ smartvoter.ino
@@ -0,0 +1,106 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+#define S1 6
+#define S2 5
+#define S3 4
+#define S4 3
+int vote1 = 0;
+int vote2 = 0;
+int vote3 = 0;
+void setup()
+{
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+  pinMode(S3, INPUT);
+  pinMode(S4, INPUT);
+  lcd.begin(16, 2);
+  lcd.print(" Electronic ");
+  lcd.setCursor(0, 1);
+  lcd.print(" Voting Machine ");
+  delay(4000);
+  digitalWrite(S1, HIGH);
+  digitalWrite(S2, HIGH);
+  digitalWrite(S3, HIGH);
+  digitalWrite(S4, HIGH);
+  lcd.clear();
+  lcd.setCursor(5, 0);
+  lcd.print("A");
+  lcd.setCursor(9, 0);
+  lcd.print("B");
+  lcd.setCursor(13, 0);
+  lcd.print("C");
+  //Serial.begin(9600);
+}
+void loop()
+{
+  lcd.setCursor(5, 0);
+  lcd.print("A");
+  lcd.setCursor(5, 1);
+  lcd.print(vote1);
+  lcd.setCursor(9, 0);
+  lcd.print("B");
+  lcd.setCursor(9, 1);
+  lcd.print(vote2);
+  lcd.setCursor(13, 0);
+  lcd.print("C");
+  lcd.setCursor(13, 1);
+  lcd.print(vote3);
+  if (digitalRead(S1) == 0)
+    vote1++;
+  while (digitalRead(S1) == 0);
+  if (digitalRead(S2) == 0)
+    vote2++;
+  while (digitalRead(S2) == 0);
+  if (digitalRead(S3) == 0)
+    vote3++;
+  while (digitalRead(S3) == 0);
+  //Serial.print("This is  working");
+  if (digitalRead(S4) == 0)
+  {
+    int vote = vote1 + vote2 + vote3;
+    if (vote)
+    {
+      if (vote1 > vote2 && vote1 > vote3)
+      {
+        lcd.clear();
+        lcd.print("A is Winner");
+        delay(3000);
+        lcd.clear();
+      }
+      else if (vote2 > vote1 && vote2 > vote3)
+      {
+        lcd.clear();
+        lcd.print("B is Winner");
+        delay(3000);
+        lcd.clear();
+      }
+      else if (vote3 > vote1 && vote3 > vote2)
+      {
+        lcd.setCursor(0, 0);
+        lcd.clear();
+        lcd.print("C is Winner");
+        delay(3000);
+        lcd.clear();
+      }
+      else
+      {
+        lcd.clear();
+        lcd.print(" Tie Up Or ");
+        lcd.setCursor(0, 1);
+        lcd.print(" No Result ");
+        delay(3000);
+        lcd.clear();
+      }
+    }
+    else
+    {
+      lcd.clear();
+      lcd.print("No Voting....");
+      delay(3000);
+      lcd.clear();
+    }
+    vote1 = vote2 = vote3 = vote = 0;
+    lcd.clear();
+  }
+}
